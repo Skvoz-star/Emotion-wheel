2c29f1d9892f3d04f4a6d1fcaaae41e008599e3c
Skvoz-star <vavilinsv@gmail.com>
2026-05-12 00:45:04 +0200

    Add service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open('emotion-wheel-v1').then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
